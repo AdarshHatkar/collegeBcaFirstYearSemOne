147780b214331c3ecbf84b7437bd2a800a6267b3
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2022-12-12 05:02:29 +0530

    p4 completed

--- dsPracticals/p_4.c
+++ dsPracticals/p_4.c
@@ -3,23 +3,39 @@ Implementation of traversing technique in array
 */
 
 #include <stdio.h>
+void scanArr(int*,int);
 void traverseArr(int*,int);
 void main()
 {
 
-int arr[10]={3,2,1,5},sizeOFArr;
+int arr[5],sizeOFArr;
+
+
 
 sizeOFArr = sizeof(arr)/sizeof(arr[0]);
+
+scanArr(&arr,sizeOFArr);
 
 traverseArr(&arr,sizeOFArr);
  
 
     
 }
-void traverseArr(int *arr,int size){
+void scanArr(int *arr,int size){
 
    
    
+   for (int i = 0; i < size; i++ ,arr++)
+   {
+    printf("enter value of arr[%i] \n",i);
+    scanf("%d",arr);
+   }
+   
+}
+void traverseArr(int *arr,int size){
+
+   
+   printf("Result of traversing \n \n");
    for (int i = 0; i < size; i++ ,arr++)
    {
     printf("%d \n",*arr);
