db12c93e8b8f6497706a7a6768a658e52cc6a058
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2023-01-13 22:07:33 +0530

    program 4 completed

---
+++ ishwarPdf/p4.c
@@ -0,0 +1,14 @@
+/*
+WAP to calculate square and cube of any number.
+*/
+#include<stdio.h>
+void main(){
+ int n;
+    // taking input from User
+    printf("Enter Number\n");
+    scanf("%d",&n);
+    // Printing square and cube
+    printf("\nSquare of %d = %d \n",n,n*n);
+    printf("Cube of %d = %d \n",n,n*n*n);
+
+}
