d1b89ce3122ab405f533398c31c8d7872bdfedfc
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2023-01-13 22:01:23 +0530

    p3 Completed

---
+++ ishwarPdf/p3.c
@@ -0,0 +1,20 @@
+/*
+WAP for printing area of rectangle.
+*/
+
+#include <stdio.h>
+void main()
+{
+    /*
+       formula:
+       Area = Hight x Length
+       */
+    int a, h, l;
+    printf("Enter Height of Rectangle in cm \n");
+    scanf("%d", &h);
+    printf("Enter Length of Rectangle in cm \n");
+    scanf("%d", &l);
+
+    a = l * h;
+    printf("area = %d cm Square", a);
+}
