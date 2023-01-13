b5bf2eb4302380264aa30f464be702cdc435bdf1
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2023-01-13 23:05:47 +0530

    p13 complete

---
+++ ishwarPdf/p13.c
@@ -0,0 +1,21 @@
+/*
+WAP for printing following pattern.
+       *
+       * *
+       * * *
+       * * * *
+
+*/
+
+#include <stdio.h>
+void main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+}
