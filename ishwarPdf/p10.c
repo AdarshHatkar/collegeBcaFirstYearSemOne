e73e1e8845dbd2b27d09bc3c8e83dbc959c0f01d
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2023-01-13 22:46:31 +0530

    p9 complete

---
+++ ishwarPdf/p10.c
@@ -0,0 +1,16 @@
+/*
+WAP to calculate total marks and percentage of a student.
+*/
+/*
+Logic:
+we taking marks for 3 subject out of 100
+and do further calculation according to this 
+Formula:
+Percentage = (obtained marks / total marks ) * 100
+
+*/
+
+#include <stdio.h>
+void main(){
+    
+}
